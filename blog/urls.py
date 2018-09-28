ce4e0a710d3f617df065ca6fb0130cb6f830e84c
Robert <juanrob_10@hotmail.com>
2018-09-28 11:50:18 -0700

    commit

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,23 @@
 from django.urls import path
 from . import views
+from .views import(
+     PostListView,
+     PostDetailView,
+     PostCreateView,
+     PostUpdateView,
+     PostDeleteView,
+     UserPostListView
+)
 
 urlpatterns = [
-    path('',views.home,name='blog-home'),
+    path('',PostListView.as_view(),name='blog-home'),
+    path('user/<str:username>',UserPostListView.as_view(),name='user-posts'),
+    path('post/<int:pk>/',PostDetailView.as_view(),name='post-detail'),
+    #CreateView espera  que haya un archivo llamado post_form para su despligue
+    path('post/new/',PostCreateView.as_view(),name='post-create'),
+    path('post/<int:pk>/update',PostUpdateView.as_view(),name='post-update'),
+    path('post/<int:pk>/delete',PostDeleteView.as_view(),name='post-delete'),
     path('about/',views.about,name='blog-about')
     
 ]
+
--- blog/views.py
+++ blog/views.py
@@ -1,22 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from django.views.generic import ListView,DetailView,CreateView,UpdateView,DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
+from django.contrib.auth.models import User
 from .models import Post
-
-posts = [
-    {
-        'autor' : 'Robert',
-        'titulo': 'Blog Post 1',
-        'contenido': 'Primer post content',
-        'fecha_post': 'Agosto 27,2018'
-    },
-        {
-        'autor' : 'Luis',
-        'titulo': 'Blog Post 2',
-        'contenido': 'Segundo post content',
-        'fecha_post': 'Agosto 27,2018'
-    }
-]
-
-
 
 
 def home(request):
@@ -25,6 +11,63 @@ def home(request):
     }
     return render(request,'blog/home.html',context)
     
+class PostListView(ListView):
+    model = Post
+    template_name ="blog/home.html"
+    context_object_name= 'posts'
+    ordering= ['-fecha_posted']
+    paginate_by=5
+
+class UserPostListView(ListView):
+    model = Post
+    template_name ="blog/user_posts.html"
+    context_object_name= 'posts'
+    paginate_by=5    
+
+    def get_queryset(self):
+        user= get_object_or_404(User,username=self.kwargs.get("username"))
+        return Post.objects.filter(autor=user).order_by('-fecha_posted')
+
+
+        
+
+
+class PostDetailView(DetailView):
+    model = Post
+    
+class PostCreateView(LoginRequiredMixin,CreateView):
+    model = Post
+    fields = ['titulo','contenido']
+
+    def form_valid(self,form):
+            form.instance.autor = self.request.user
+            return super().form_valid(form)
+            
+
+class PostUpdateView(LoginRequiredMixin,UserPassesTestMixin,UpdateView):
+    model = Post
+    fields = ['titulo','contenido']
+
+    def form_valid(self, form):
+            form.instance.autor = self.request.user
+            return super().form_valid(form)
+
+    def test_func(self):
+            post = self.get_object()
+            if self.request.user == post.autor:
+                return True
+            return False
+
+class PostDeleteView(LoginRequiredMixin,UserPassesTestMixin,DeleteView):
+    model = Post
+    success_url="/"    
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.autor:
+            return True
+        return False        
+
 def about(request):
     return render(request,'blog/about.html',{ 'titulo': 'About'})
 
